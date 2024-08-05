5be57a36fe441970908999be2036fb09e4241892
PPPoria <1316543327@qq.com>
2024-08-05 11:39:12 +0800

    init

---
+++ monitor/src/main/java/com/example/monitor/QGExemptionHandler.java
@@ -0,0 +1,10 @@
+package com.example.monitor;
+
+import androidx.annotation.NonNull;
+
+public class QGExemptionHandler implements Thread.UncaughtExceptionHandler {
+    @Override
+    public void uncaughtException(@NonNull Thread t, @NonNull Throwable e) {
+
+    }
+}
